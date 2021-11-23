7bf624be59c1f7f1d3114533ed8e91af2d20d93a
Matheus Moreira Luna <matheusluna96@gmail.com>
2021-11-22 23:11:40 -0300

    adicionando projeto do capitulo

---
+++ js/init.js
@@ -0,0 +1,5 @@
+(function(){
+    var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+    i.src="https://cdn.curator.io/published/1a1df1c6-15c9-4807-9755-aefb783e39ae.js";
+    e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
